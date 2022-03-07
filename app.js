d7a0c0710389f0e164867a64b2f91d53932e3b23
MiriamTostado <2019030404@upsin.edu.mx>
2022-03-07 16:18:31 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+ 
+
+app.get('/', (req,res)=>{
+    res.send("iniciamos Servidor");
+
+})
+
+
+const puerto = 3000 ;
+app.listen(puerto,()=>{
+    console.log("Escuchando");
+
+});
+
+
